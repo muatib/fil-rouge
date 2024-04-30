685677b4d9423aa570e2202ea9d5568758a8e29c
dubois <vindubois14@gmail.com>
2024-04-30 17:51:24 +0200

    burger menu

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
+const burgerMenu = document.getElementById('burger__menu');
+const overlay = document.getElementById('menu')
 
+burgerMenu.addEventListener('click', function(){
+    this.classList.toggle("close");
+    overlay.classList.toggle("overlay");
+})
 
